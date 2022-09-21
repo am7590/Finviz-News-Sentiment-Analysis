4bf2d0b7783543141241e86549618557b84e488d
Alek Michelson <am7590@rit.edu>
2022-09-20 21:48:47 -0400

    refactor

--- main.py
+++ main.py
@@ -8,23 +8,6 @@ from operator import itemgetter
 from collections import Counter
 
 
-def print_full_dataframe(df, head):
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        # Display just the last few lines or the whole df?
-        if head:
-            print(df.head())
-        else:
-            print(df)
-
-
-def chart_mean_compound_scores(mean_df):
-    # Chart average compound scores with matplotlib
-    mean_df = mean_df.unstack()
-    mean_df = mean_df.xs('compound', axis="columns").transpose()
-    mean_df.plot(kind='bar')
-    plt.show()
-
-
 def get_date(s):
     return datetime.datetime.fromtimestamp(s).strftime('%Y-%m-%d %H:%M:%S.%f')
 
@@ -41,7 +24,6 @@ def flatten(l):
 def get_final_data(ticker):
     url = f'https://cloud.iexapis.com/stable/stock/{ticker}/news/last/100/?token={token}'
     data = requests.get(url).json()
-    print(data)
     sentiment = SentimentIntensityAnalyzer()
 
     date_list = []
@@ -49,16 +31,13 @@ def get_final_data(ticker):
         date_list.append(item['datetime'])
 
     dates = [get_date(i/1000) for i in date_list]
-    print(dates)
-
-    day_lists = [[] for _ in range(5)]  # [[], [], ..., []]
+    day_lists = [[] for _ in range(5)]
     hour_lists = [[] for _ in range(10)]
     current_date = ''
     current_list_index = 0
     key_count = len(Counter([item.split('-')[1] for item in dates]).keys())
-    print(key_count)
     if key_count < 5:
-        print("splitting dates by hour of last day")
+        # splitting dates by hour of last day
         for item in dates:
             if len(current_date) == 0:
                 current_date = item.split(' ')[1][0:3]
@@ -72,7 +51,7 @@ def get_final_data(ticker):
                 current_date = item.split(' ')[1][0:3]
                 hour_lists[current_list_index].append(item)
     else:
-        print("split by date")
+        # split by date
         for item in dates:
             if len(current_date) == 0:
                  current_date = item.split('-')[1]+item.split('-')[2][0:3]
@@ -86,45 +65,30 @@ def get_final_data(ticker):
                 current_date = item.split('-')[1]+item.split('-')[2][0:3]
                 day_lists[current_list_index].append(item)
 
-    print(hour_lists)
-    print(day_lists)
-
     find_sentiment_list = []
 
     for item in data:
         for hour_list in hour_lists:
             for hour in hour_list:
-            # print(get_date(item['datetime']/1000))
-            # print(hour)
                 if get_date(item['datetime']/1000) == hour:
                     find_sentiment_list.append(item)
-                    print(item)
 
     for item in data:
         for day_list in day_lists:
             for day in day_list:
                 if get_date(item['datetime'] / 1000) == day:
                     find_sentiment_list.append(item)
-                    print(item)
 
     sentiment_score_list = []
     for item in find_sentiment_list:
         sentiment_score_list.append(sentiment.polarity_scores(item['summary'])['compound'])
 
-    print(sentiment_score_list)
-    # print([len(x) for x in sentiment_score_list])
-
     final_score_list = []
     type = ''
     if len(hour_lists[0]) > 0:
-        final_score_list = hour_lists
         type = 'hours'
     else:
-        final_score_list = day_lists
         type = "days"
-
-    print(final_score_list)
-    print([len(x) for x in final_score_list])
 
     return dict(zip(flatten(final_score_list), sentiment_score_list)), type
 
